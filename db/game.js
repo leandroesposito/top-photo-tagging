602a3efed69728f502521ef21db473dd6c48ed81
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-12-13 21:10:36 -0300

    Create db game controller

---
+++ db/game.js
@@ -0,0 +1,40 @@
+const { runQuery } = require("./queries");
+
+async function getAllGames() {
+  const query = "SELECT id, name, thumbnail FROM games";
+
+  return await runQuery(query);
+}
+
+async function getGameWithObjectives(id) {
+  const gameQuery = `
+    SELECT
+      id,
+      name,
+      credits,
+      pictureFilename
+    FROM
+      games
+    WHERE
+      id=$1;
+  `;
+  const gameParams = [id];
+
+  const game = (await runQuery(gameQuery, gameParams))[0];
+
+  const objectivesQuery = `
+    SELECT id, name, pictureFilename FROM objectives WHERE game_id=$1;
+  `;
+  const objectivesParams = [id];
+
+  const objectives = await runQuery(objectivesQuery, objectivesParams);
+
+  game.objectives = objectives;
+
+  return game;
+}
+
+module.exports = {
+  getAllGames,
+  getGameWithObjectives,
+};
