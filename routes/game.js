80104bfa60fe7903406619e9ab41dd6f36478ab3
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-12-16 18:07:24 -0300

    Add get game details route

--- routes/game.js
+++ routes/game.js
@@ -3,6 +3,7 @@ const gameController = require("../controllers/game");
 
 const gameRouter = Router();
 
+gameRouter.get("/:gameId", gameController.getGameWithObjectives);
 gameRouter.get("/", gameController.getAllGames);
 
 module.exports = gameRouter;
