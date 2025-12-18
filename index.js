ec8709d92bc5cb892ac1f991ef23c65cf5246979
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-12-17 23:59:56 -0300

    Use try router

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const gameRouter = require("./routes/game");
 const cors = require("cors");
+const gameRouter = require("./routes/game");
+const tryRouter = require("./routes/try");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/games", gameRouter);
+app.use("/try", tryRouter);
 
 const port = process.env.SERVER_PORT || 3000;
 app.listen(port, (error) => {
